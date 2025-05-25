fb422caa3adfaa9c97b48b61392d81969530cae2
berserk-reasoned <jay.24sece1020029@galgotiasuniversity.ac.in>
2025-05-25 22:48:20 +0530

    Update EncryptionService.java

--- src/main/java/com/encryptor/service/EncryptionService.java
+++ src/main/java/com/encryptor/service/EncryptionService.java
@@ -4,7 +4,8 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.io.File;
 import java.nio.file.Files;
 import java.security.SecureRandom;
 import java.util.Base64;
@@ -44,15 +45,32 @@ public class EncryptionService {
         byte[] fileData = Files.readAllBytes(inputFile.toPath());
         
         KeyGenerator keyGen = KeyGenerator.getInstance(method.getAlgorithm());
-        keyGen.init(getKeySize(method));
+        keyGen.init(getKeySize(method), new SecureRandom());
         SecretKey secretKey = keyGen.generateKey();
         
-        Cipher cipher = Cipher.getInstance(method.getAlgorithm());
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        String transformation = getTransformation(method);
+        Cipher cipher = Cipher.getInstance(transformation);
+        
+        byte[] iv = null;
+        if (transformation.contains("CBC")) {
+            iv = new byte[cipher.getBlockSize()];
+            new SecureRandom().nextBytes(iv);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
+        } else {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        }
         
         byte[] encryptedData = cipher.doFinal(fileData);
-        String encodedKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
         
+        // Prepend IV to encrypted data if using CBC
+        if (iv != null) {
+            byte[] combined = new byte[iv.length + encryptedData.length];
+            System.arraycopy(iv, 0, combined, 0, iv.length);
+            System.arraycopy(encryptedData, 0, combined, iv.length, encryptedData.length);
+            encryptedData = combined;
+        }
+        
+        String encodedKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
         return new EncryptionResult(encryptedData, encodedKey);
     }
     
@@ -60,10 +78,28 @@ public class EncryptionService {
         byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
         SecretKey secretKey = new SecretKeySpec(decodedKey, method.getAlgorithm());
         
-        Cipher cipher = Cipher.getInstance(method.getAlgorithm());
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        String transformation = getTransformation(method);
+        Cipher cipher = Cipher.getInstance(transformation);
         
+        if (transformation.contains("CBC")) {
+            int blockSize = cipher.getBlockSize();
+            byte[] iv = new byte[blockSize];
+            System.arraycopy(encryptedData, 0, iv, 0, blockSize);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+            return cipher.doFinal(encryptedData, blockSize, encryptedData.length - blockSize);
+        }
+        
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
         return cipher.doFinal(encryptedData);
+    }
+    
+    private String getTransformation(EncryptionMethod method) {
+        switch (method) {
+            case AES: return "AES/CBC/PKCS5Padding";
+            case DES: return "DES/ECB/PKCS5Padding";
+            case BLOWFISH: return "Blowfish/CBC/PKCS5Padding";
+            default: return method.getAlgorithm() + "/CBC/PKCS5Padding";
+        }
     }
     
     private int getKeySize(EncryptionMethod method) {
