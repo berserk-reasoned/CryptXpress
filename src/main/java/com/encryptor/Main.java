1074735c78dfe6ed88a626f36ba0d811934d5468
berserk-reasoned <jay.24sece1020029@galgotiasuniversity.ac.in>
2025-05-25 19:52:47 +0530

    Update Main.java

--- src/main/java/com/encryptor/Main.java
+++ src/main/java/com/encryptor/Main.java
@@ -1,1 +1,20 @@
+package com.encryptor;
 
+import com.encryptor.ui.MainFrame;
+import com.formdev.flatlaf.FlatDarkLaf;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        SwingUtilities.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
+    }
+}
