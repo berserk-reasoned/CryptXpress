ac17f92f2cb525987ea16f06f2f9444367beeaac
berserk-reasoned <jay.24sece1020029@galgotiasuniversity.ac.in>
2025-05-25 19:55:41 +0530

    Update DatabaseConnection.java

--- src/main/java/com/encryptor/util/DatabaseConnection.java
+++ src/main/java/com/encryptor/util/DatabaseConnection.java
@@ -1,1 +1,35 @@
+package com.encryptor.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/file_encryptor_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "your_password";
+    
+    private static Connection connection;
+    
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("MySQL JDBC Driver not found", e);
+            }
+        }
+        return connection;
+    }
+    
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
